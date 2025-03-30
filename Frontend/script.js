4a7c6971172755d1da8fdcc84a8dc48d25e8003d
subha123-ux <subhajitmal509@gmail.com>
2025-03-30 10:56:09 +0530

    ShopEase Project

--- Frontend/script.js
+++ Frontend/script.js
@@ -84,7 +84,8 @@ function displayProducts(productList = products) {
         button.style.backgroundColor="#f1e7e7";
         button.addEventListener("click", function () {
             // seemore(product.id);
-            window.location.href = "electronics.html";
+            // window.location.href = "electronics.html";
+            window.location.href=product.category+".html";
         });
 
         div.appendChild(button);
